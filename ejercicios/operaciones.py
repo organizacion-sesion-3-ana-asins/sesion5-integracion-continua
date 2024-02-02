176341003bef8c775ab66e10df8e4ecf6162d8e0
Ana Asins Aleixandre <a.asinsaleixandre@edu.gva.es>
2024-02-02 12:18:55 +0100

    es par

--- ejercicios/operaciones.py
+++ ejercicios/operaciones.py
@@ -3,3 +3,9 @@ def suma(x,y):
 
     # El alumno deberá implementar correctamente el código de la función para pasar el test correspondiente.
     return x + y
+
+def esPar(x):
+    if x%2 == 0:
+        return "par"
+    else:
+        return "impar"
